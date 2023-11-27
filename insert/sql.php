45ba1b4465ad492fe32489ad6cb888925c6dc1c0
NgurahNova <91045890+NgurahNova@users.noreply.github.com>
2023-11-27 19:48:29 +0800

    Add files via upload

---
+++ insert/sql.php
@@ -0,0 +1,10 @@
+<?php
+
+	$server		= "localhost"; 
+	$user		= "root";
+	$password	= ""; 
+	$database	= "2101020019_ngurah nova"; 
+
+	$con = mysqli_connect($server,$user,$password,$database);
+
+?>
